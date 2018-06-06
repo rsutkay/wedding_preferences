dffd2a7e2d07733762015df1155377b27215325b
Raghu Betina <raghu@firstdraft.com>
2018-06-06 04:23:58 +0000

    Validate uniqueness of users.username

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
 
   # Validations
 
+  validates :username, :uniqueness => true
+
   validates :username, :presence => true
 
   validates :username, :length => { :minimum => 6, :maximum => 12 }
