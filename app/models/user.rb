8dc6b13665b6d9d89a861cbf584eabfcd1e4b150
Raghu Betina <raghu@firstdraft.com>
2018-06-06 04:23:58 +0000

    Validate length of users.username

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
 
   # Validations
 
+  validates :username, :length => { :minimum => 6, :maximum => 12 }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
