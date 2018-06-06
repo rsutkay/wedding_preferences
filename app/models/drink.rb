837f8404bf58c6bef88152fd70ca5db4b3b4d18c
Raghu Betina <raghu@firstdraft.com>
2018-06-06 04:23:58 +0000

    Validate presence of drinks.brand_id

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -5,6 +5,8 @@ class Drink < ApplicationRecord
 
   # Validations
 
+  validates :brand_id, :presence => true
+
   validates :category_id, :presence => true
 
   validates :name, :uniqueness => { :scope => [:brand_id, :category_id] }
